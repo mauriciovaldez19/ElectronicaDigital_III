657d83f9b72e2cd9a5559098867f2cd20106587e
Mauricio L Valdez Benavidez <mvaldez@mi.unc.edu.ar>
2023-11-16 01:58:14 -0300

    Update Luxometro.c
    Co-Authored-By: AndyTaborda <102503527+AndyTaborda@users.noreply.github.com>

--- TP_FINAL/Luxometro.c
+++ TP_FINAL/Luxometro.c
@@ -35,25 +35,46 @@ int main(void){
     cfgTIMER();
     cfgEINT();
 
+PINSEL_CFG_Type led_Red;
+    led_Red.Portnum = PINSEL_PORT_0;
+    led_Red.Pinnum = PINSEL_PIN_22;
+    led_Red.Funcnum = PINSEL_FUNC_0;
+    led_Red.Pinmode = PINSEL_PINMODE_PULLUP;
+    led_Red.OpenDrain = PINSEL_PINMODE_NORMAL;
+PINSEL_ConfigPin(&led_Red);
+GPIO_SetDir(PINSEL_PORT_0, PINSEL_PIN_22,1);
+
+
     while(1){}
 
     return 0;
 }
+void EINT0_IRQHandler(void){
+	static uint8_t state=0;
+    state++;
+    if(state%2){
+        GPIO_SetValue(PINSEL_PORT_0, PINSEL_PIN_22);
+    }
+    else{
+        GPIO_ClearValue(PINSEL_PORT_0, PINSEL_PIN_22);
+    }
+	return;
+}
 void cfgEINT(void){
+PINSEL_CFG_Type ext_int_cfg;
+    ext_int_cfg.Portnum = PINSEL_PORT_2;
+    ext_int_cfg.Pinnum = PINSEL_PIN_10;
+    ext_int_cfg.Funcnum = PINSEL_FUNC_1;
+    ext_int_cfg.Pinmode = PINSEL_PINMODE_PULLUP;
+    ext_int_cfg.OpenDrain = PINSEL_PINMODE_NORMAL;
+PINSEL_ConfigPin(&ext_int_cfg);  
 EXTI_InitTypeDef ext_int;
     ext_int.EXTI_Line = EXTI_EINT0;
     ext_int.EXTI_Mode = EXTI_MODE_EDGE_SENSITIVE;
     ext_int.EXTI_polarity = EXTI_POLARITY_LOW_ACTIVE_OR_FALLING_EDGE;
     EXTI_Config(&ext_int);
     EXTI_ClearEXTIFlag(EXTI_EINT0);
-/*PINSEL_CFG_Type ext_int;
-    PinCfg.Portnum = PINSEL_PORT_2;
-    PinCfg.Pinnum = PINSEL_PIN_10;
-    PinCfg.Funcnum = PINSEL_FUNC_1;
-    PinCfg.Pinmode = PINSEL_PINMODE_PULLUP;
-    PinCfg.OpenDrain = PINSEL_PINMODE_NORMAL;
-PINSEL_ConfigPin(&ext_int);  
-*/
+    NVIC_EnableIRQ(EINT0_IRQn);
     return;
 }
 void cfgTIMER(void){
@@ -72,7 +93,7 @@ ADC_Init(LPC_ADC, 200000);
 LPC_ADC->ADGDR &= ~(1<<31);
 ADC_ChannelCmd(LPC_ADC,ADC_CHANNEL_0,ENABLE);
 
-return
+return;
 }
 void cfgDAC(void){
 PINSEL_CFG_Type dacPin_cfg;
@@ -87,20 +108,20 @@ return;
 }
 void cfgGPIO(void){
 PINSEL_CFG_Type led_Red;
-    PinCfg.Portnum = PINSEL_PORT_2;
-    PinCfg.Pinnum = PINSEL_PIN_3;
-    PinCfg.Funcnum = PINSEL_FUNC_0;
-    PinCfg.Pinmode = PINSEL_PINMODE_PULLUP;
-    PinCfg.OpenDrain = PINSEL_PINMODE_NORMAL;
+    led_Red.Portnum = PINSEL_PORT_2;
+    led_Red.Pinnum = PINSEL_PIN_3;
+    led_Red.Funcnum = PINSEL_FUNC_0;
+    led_Red.Pinmode = PINSEL_PINMODE_PULLUP;
+    led_Red.OpenDrain = PINSEL_PINMODE_NORMAL;
 PINSEL_ConfigPin(&led_Red);
 GPIO_SetDir(PINSEL_PORT_2, PINSEL_PIN_3,1); 
 
 PINSEL_CFG_Type led_Green;
-    PinCfg.Portnum = PINSEL_PORT_2;
-    PinCfg.Pinnum = PINSEL_PIN_4;
-    PinCfg.Funcnum = PINSEL_FUNC_0;
-    PinCfg.Pinmode = PINSEL_PINMODE_PULLUP;
-    PinCfg.OpenDrain = PINSEL_PINMODE_NORMAL;
+    led_Green.Portnum = PINSEL_PORT_2;
+    led_Green.Pinnum = PINSEL_PIN_4;
+    led_Green.Funcnum = PINSEL_FUNC_0;
+    led_Green.Pinmode = PINSEL_PINMODE_PULLUP;
+    led_Green.OpenDrain = PINSEL_PINMODE_NORMAL;
 PINSEL_ConfigPin(&led_Green);
 GPIO_SetDir(PINSEL_PORT_2, PINSEL_PIN_4,1);
 
